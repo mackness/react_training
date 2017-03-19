b2235239afd5cdc47da9d77cc56a01983ad880e0
macksol <macksol@gmail.com>
2017-03-19 10:56:13 -0700

    simple async

---
+++ src/AsyncModule.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AsyncModule extends React.Component {
+  render() {
+    return (
+      <div>
+        this is async.
+      </div>
+    )
+  }
+}
+
+export default AsyncModule
